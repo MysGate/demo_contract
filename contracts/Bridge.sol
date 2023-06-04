29544e13d7f898dd95c438fe499979dd45c1b401
FleetingTimePaul <83365885@qq.com>
2023-06-04 15:07:19 +0800

    Update verifier module.

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5,12 +5,23 @@ import "./MerkleTree.sol";
 import "./verifier.sol";
 
 contract Bridge is MerkleTree ,Verifier {
+    event CommitmentAdded(uint256 indexed commitment, uint256 leafIndex, uint256 timestamp);
+    
+    address public controller;
+
     mapping (uint256 => bool) public roots;
     mapping(uint256 => bool) public commitments;
-    event CommitmentAdded(uint256 indexed commitment, uint256 leafIndex, uint256 timestamp);
-    constructor  (address _mimc) MerkleTree(_mimc) public {}
 
-    function addCommitment(uint256 _commitment) public{
+    constructor  (address _mimc, address _controller) MerkleTree(_mimc) public {
+        controller = _controller;
+    }
+    
+    modifier onlyController() {
+        require(msg.sender == controller, "");
+        _;
+    }
+
+    function addCommitment(uint256 _commitment) public onlyController {
         require(!commitments[_commitment], "The commitment has been submitted");
         uint256 insertedIndex = insert(_commitment);
         commitments[_commitment] = true;
@@ -22,9 +33,9 @@ contract Bridge is MerkleTree ,Verifier {
         uint[2] memory a,
         uint[2][2] memory b,
         uint[2] memory c,
-        uint[1] memory input) public view returns (bool) {
+        uint[2] memory input) public view returns (bool) {
         require(isKnownRoot(input[0]), "Cannot find your merkle root"); // Make sure to use a recent one
-        require(verifyProof(a, b, c, input), "Invalid withdraw proof");
+        require(verifyProof(a, b, c, input), "Invalid proof");
         return true;
     }
 
--- test/CrossController.test.js
+++ test/CrossController.test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai"); 
 const { ethers, upgrades } = require("hardhat"); 
+const cls = require("circomlibjs");
 
 describe("CrossController", function () { 
   it("Should return the correct floatRate and porterPool", async function () { 
@@ -11,13 +12,13 @@ describe("CrossController", function () {
     const CC = await CrossController.deploy();
     await CC.deployed();
     await CC.initialize(addr1.address);
-    console.log("CrossControllerAddr: ", CC.address);
+    console.log("CrossController Addr: ", CC.address);
 
     const PF = await PorterPoolFactory.deploy(CC.address);
     await PF.deployed();
   
     await CC.setPorterFactory(PF.address);
-    console.log("PF: ", PF.address);
+    console.log("porter factory Addr: ", PF.address);
 
     expect(await CC.porterFactory()).to.equal(PF.address); 
     await CC.setFloatFee(2);
@@ -25,10 +26,35 @@ describe("CrossController", function () {
 
     await PF.createPorterPool("0x");
     var porterPoolAddr = await PF.porterPools(addr1.address);
-    console.log("porterPoolAddr: ", porterPoolAddr);
+    console.log("porterPool Addr: ", porterPoolAddr);
 
     var porterPool = await PorterPool.attach(porterPoolAddr);
     await porterPool.setFixedFee(10000);
     expect(await porterPool.fixedFee()).to.equal(10000); 
+
+    var porterPool = await PorterPool.attach(porterPoolAddr);
+    await porterPool.setFixedFee(10000);
+    
+    // zk
+    poseidonHash = await cls.buildPoseidonReference();
+    const C6 = new hre.ethers.ContractFactory(
+      cls.poseidonContract.generateABI(2),
+      cls.poseidonContract.createCode(2),
+      addr1
+    );
+    poseidonContract = await C6.deploy();
+    console.log("poseidon Address:", poseidonContract.address)
+  
+    var bridgeFactory = await ethers.getContractFactory("Bridge");
+    let bridge = await bridgeFactory.deploy(poseidonContract.address, CC.address);
+    await bridge.deployed()
+    console.log("Bridge Addr: ", bridge.address);
+  
+    await CC.setZkVerifier(bridge.address);
+    expect(await CC.zkVerifier()).to.equal(bridge.address); 
+
+    expect(await CC.enable()).to.equal(false);
+    await CC.enableZkVerifier(true);
+    expect(await CC.enable()).to.equal(true); 
   }); 
 }); 
